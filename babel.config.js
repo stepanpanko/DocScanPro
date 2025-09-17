df68451191e062e00c929030e662169700b778fa
Stepan <122358164+stepanpanko@users.noreply.github.com>
2025-09-17 23:46:38 +0300

    Added some final protection so runs safe

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    // add 'nativewind/babel' here only if you actively use NativeWind
     'react-native-reanimated/plugin', // must be last
   ],
 };
