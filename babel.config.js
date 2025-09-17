88982e87a3d43597d8f846ba30468bafb0243d63
Stepan <122358164+stepanpanko@users.noreply.github.com>
2025-09-17 23:24:42 +0300

    Fixed issues with simulator and babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'nativewind/babel',
-    'react-native-reanimated/plugin', // keep last
+    'react-native-reanimated/plugin', // must be last
   ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-reanimated|react-native-gesture-handler|react-native-safe-area-context)/)',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
